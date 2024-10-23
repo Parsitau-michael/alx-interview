d52f053485c790baab16ba5ac54d7e3cb4c21e95
Parsitau-michael <parsitaumichael@gmail.com>
2024-10-23 16:54:22 +0300

    Corrected PEP8 errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,9 +2,9 @@
 """
 Pascal's Triangle Module
 
-This module provides a function to generate Pascal's Triangle, a triangular array
-of the binomial coefficients. Each number in the triangle is the sum of the two
-numbers directly above it in the previous row.
+This module provides a function to generate Pascal's Triangle, a triangular
+array of the binomial coefficients. Each number in the triangle is the sum of
+the two numbers directly above it in the previous row.
 
 Functions:
 ----------
@@ -12,21 +12,23 @@ Functions:
 
 Usage:
 ------
-To generate Pascal's Triangle for a specific number of rows, call the `pascal_triangle` 
-function with an integer argument representing the desired number of rows. 
-The function returns a list of lists, where each inner list corresponds to a row 
-in Pascal's Triangle.
+To generate Pascal's Triangle for a specific number of rows, call the
+`pascal_triangle` function with an integer argument representing the
+desired number of rows.
+The function returns a list of lists, where each inner list corresponds
+to a row in Pascal's Triangle.
 
 Example:
 --------
 >>> triangle = pascal_triangle(5)
 >>> print(triangle)
-[[1], 
-[1, 1], 
-[1, 2, 1], 
-[1, 3, 3, 1], 
+[[1],
+[1, 1],
+[1, 2, 1],
+[1, 3, 3, 1],
 [1, 4, 6, 4, 1]]
 """
+
 
 def pascal_triangle(n):
     """
@@ -37,7 +39,7 @@ def pascal_triangle(n):
              Must be a non-negative integer.
 
     Returns:
-    List[List[int]]: A list of lists containing the values of Pascal's Triangle.
+    List[List[int]]: A list of lists containing the values of Pascal Triangle.
                      Each inner list represents a row in the triangle.
                      Returns an empty list if n is less than or equal to 0.
     """
@@ -48,7 +50,7 @@ def pascal_triangle(n):
 
     for i in range(1, n):
         row = [1] + [
-            triangle[i - 1][j - 1] + triangle[i - 1][j] 
+            triangle[i - 1][j - 1] + triangle[i - 1][j]
             for j in range(1, i)
             ] + [1]
         triangle.append(row)
