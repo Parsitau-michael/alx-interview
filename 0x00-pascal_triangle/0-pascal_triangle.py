0d01c2d2645a55d6c74d417c1595234bf0190ab9
Parsitau-michael <parsitaumichael@gmail.com>
2024-10-23 16:40:57 +0300

    Edited my module documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,32 @@
 #!/usr/bin/python3
-# This module represents a function that returns the pascal triangle.
+"""
+Pascal's Triangle Module
+
+This module provides a function to generate Pascal's Triangle, a triangular array
+of the binomial coefficients. Each number in the triangle is the sum of the two
+numbers directly above it in the previous row.
+
+Functions:
+----------
+- pascal_triangle(n): Generates Pascal's Triangle up to the n-th row.
+
+Usage:
+------
+To generate Pascal's Triangle for a specific number of rows, call the `pascal_triangle` 
+function with an integer argument representing the desired number of rows. 
+The function returns a list of lists, where each inner list corresponds to a row 
+in Pascal's Triangle.
+
+Example:
+--------
+>>> triangle = pascal_triangle(5)
+>>> print(triangle)
+[[1], 
+[1, 1], 
+[1, 2, 1], 
+[1, 3, 3, 1], 
+[1, 4, 6, 4, 1]]
+"""
 
 def pascal_triangle(n):
     if (n <= 0):
@@ -8,8 +35,10 @@ def pascal_triangle(n):
     triangle = [[1]]
 
     for i in range(1, n):
-        e = [triangle[i - 1][j - 1] + triangle[i - 1][j] for j in range(1, i)]
-        row = [1] + e + [1]
+        row = [1] + [
+            triangle[i - 1][j - 1] + triangle[i - 1][j] 
+            for j in range(1, i)
+            ] + [1]
         triangle.append(row)
 
     return triangle
