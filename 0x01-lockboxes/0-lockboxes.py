406115749a6979d6bc276f1ca718d98ddfb5b7b0
Parsitau-michael <parsitaumichael@gmail.com>
2024-10-26 20:58:01 +0300

    Corrected pycodestyle errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,15 +10,15 @@ found within them.
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked starting from box 0.
-    
+
     Parameters:
     - boxes (list of lists): A list where each index represents a box,
      and each box contains a list of keys to other boxes.
      Each key is a positive integer corresponding to a box number.
-    
+
     Returns:
     - bool: True if all boxes can be opened, otherwise False.
-    
+
     Logic:
     - Begin with the first box (index 0) unlocked.
     - Explore each box’s keys iteratively, adding new boxes unlocked to a set.
@@ -28,13 +28,13 @@ def canUnlockAll(boxes):
     """
 
     openedBoxes = {0}
-    boxes_to_check =[0]
+    boxes_to_check = [0]
 
     while boxes_to_check:
         current_box = boxes_to_check.pop()
         for key in boxes[current_box]:
-            if not key in openedBoxes and key < len(boxes):
+            if key not in openedBoxes and key < len(boxes):
                 openedBoxes.add(key)
                 boxes_to_check.append(key)
-    
+
     return len(openedBoxes) == len(boxes)
